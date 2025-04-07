80985adb69a1a7fe0c9b62d1ee4eef707e1973f1
Majed Al-Shuwaiee <m.shuwaiee@gmail.com>
2025-04-07 17:31:51 +0300

    Initial commit

---
+++ src/main/java/com/coded/spring/springboot/KotlinSpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.coded.spring.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotlinSpringbootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KotlinSpringbootProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coded/spring/springboot/KotlinSpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coded.spring.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KotlinSpringbootProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
