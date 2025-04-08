f03e71fe3e6b33b035c6daad74959018573f1d06
fatemahAlMomen <fatemah.alm@hotmail.com>
2025-04-08 10:47:49 +0300

    Welcome page Get

---
+++ src/main/java/com/coded/spring/springboot/OnlineOrderController.kt
@@ -0,0 +1,11 @@
+package com.coded.spring.springboot
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class OnlineOrderController(
+){
+    @GetMapping("/home")
+    fun onlineOrder() =  "Start Ordering Food!"
+}
